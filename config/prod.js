06c3c6763b58e65f5aaefe02da999f999ba61a11
dq.tho89 <dq.tho89@gmail.com>
2017-11-29 13:22:02 +0700

    Add config variables in Heroku

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//  24514341406-00349j7vju7fn0o5aj88ssfb83o3u242.apps.googleusercontent.com 
+//  YwD2b4jHllTrB-ZsNK-jW0tz 
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURL: process.env.MONGO_URL,
+  cookieKey: process.env.COOKIE_KEY
+}
